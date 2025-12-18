a415b8147dfaa48880942005a5f647fff08099c4
JhonSnow <jhon.vallecilla@alumnos.ucn.cl>
2025-12-18 12:59:04 -0300

    docs: finalize error message documentation

--- Cubitwelve/Src/Common/Constants/ErrorMessages.cs
+++ Cubitwelve/Src/Common/Constants/ErrorMessages.cs
@@ -12,16 +12,34 @@ namespace Cubitwelve.Src.Common.Constants
         /// </summary>
         public const string InvalidCredentials = "Invalid credentials";
 
+        /// <summary>
+        /// Entity not deleted message
+        /// </summary>
         public const string EntityNotDeleted = "Entity not deleted";
 
+        /// <summary>
+        /// Internal server error message
+        /// </summary>
         public const string InternalServerError = "Internal error - Please contact the administrator";
 
+        /// <summary>
+        /// Duplicate user message
+        /// </summary>
         public const string DuplicateUser = "Duplicate user";
 
+        /// <summary>
+        /// Disabled user message
+        /// </summary>
         public const string DisabledUser = "User is not enabled - Contact an administrator";
 
+        /// <summary>
+        /// Unauthorized access message
+        /// </summary>
         public const string UnauthorizedAccess = "Unauthorized access";
         
+        /// <summary>
+        /// Entity duplicated message
+        /// </summary>
         public const string EntityDuplicated = "The entity already exists";
     }
 }
